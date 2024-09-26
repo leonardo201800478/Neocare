d44ea0c42f08dc97d0f808382950e4cb05db7287
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2024-09-26 16:30:00 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,22 @@
-import { Ionicons } from '@expo/vector-icons';
+// app/index.tsx
 import { useRouter } from 'expo-router';
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
 
-const HomePage = () => {
+const Index = () => {
   const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Bem-vindo ao Sistema de Gerenciamento</Text>
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-      <TouchableOpacity style={styles.button} onPress={() => router.push(`./doctors`)}>
-        <Ionicons name="person-outline" size={24} color="#fff" />
-        <Text style={styles.buttonText}>Gerenciar Médicos</Text>
-      </TouchableOpacity>
+  useEffect(() => {
+    if (isMounted) {
+      router.replace('/auth/Login'); // Redireciona para a tela de Login
+    }
+  }, [isMounted, router]);
 
-      <TouchableOpacity style={styles.button} onPress={() => router.push('./patients')}>
-        <Ionicons name="people-outline" size={24} color="#fff" />
-        <Text style={styles.buttonText}>Gerenciar Pacientes</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={() => router.push('./attendences')}>
-        <Ionicons name="document-text-outline" size={24} color="#fff" />
-        <Text style={styles.buttonText}>Gerenciar Atendimentos</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return null;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-    backgroundColor: '#f4f4f4',
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 32,
-    textAlign: 'center',
-  },
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#A700FF',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    marginBottom: 16,
-    width: '80%',
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#fff',
-    marginLeft: 8,
-  },
-});
-
-export default HomePage;
+export default Index;
