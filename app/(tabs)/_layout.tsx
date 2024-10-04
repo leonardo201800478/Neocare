c28571c8c30e352980c113e279c58a5a00c00f38
Leonardo <201800478@unifoa.edu.br>
2024-10-04 16:56:22 -0300

    Estilizando a TAB transparente

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,13 +5,27 @@ import React from 'react';
 
 export default function AppTabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: '#A020F0',
+        tabBarInactiveTintColor: '#000000', // Cor da aba não selecionada
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute', // Posiciona a tab bar de forma absoluta
+          backgroundColor: 'transparent', // Torna o fundo transparente
+          borderTopWidth: 0, // Remove a borda superior
+          elevation: 0, // Remove a sombra no Android
+          shadowOpacity: 0, // Remove a sombra no iOS
+          height: 55, // Ajusta a altura para maior personalização (opcional)
+        },
+      }}>
       {/* Aba Home */}
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          title: 'Pesquisa',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
         }}
       />
       {/* Aba Doctors */}
@@ -22,19 +36,20 @@ export default function AppTabsLayout() {
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-md" color={color} />,
         }}
       />
-      {/* Aba Graficos */}
+      {/* Aba Gráficos */}
       <Tabs.Screen
         name="graficos"
         options={{
-          title: 'Graficos',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-md" color={color} />,
+          title: 'Gráficos',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="info-circle" color={color} />,
         }}
       />
+      {/* Aba Screens */}
       <Tabs.Screen
         name="screens"
         options={{
           title: 'Tratamentos',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-md" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="stethoscope" color={color} />,
         }}
       />
     </Tabs>
