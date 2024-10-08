7288dec17c0a10a4e9498dc5f864848660b667de
Leonardo Paiva <67243028+leonardo201800478@users.noreply.github.com>
2024-10-08 14:26:17 -0300

    Create termos.tsx

---
+++ termos.tsx
@@ -0,0 +1,49 @@
+Termos de Consentimento e Aceite para o Uso de Dados em Neocare
+1. Introdução
+O aplicativo Neocare ("Aplicativo") é uma ferramenta desenvolvida para auxiliar médicos e profissionais de saúde no atendimento e acompanhamento de pacientes neonatais. A proteção dos dados pessoais, especialmente os dados sensíveis de saúde, é de extrema importância. Por isso, este termo de consentimento segue os princípios da Lei Geral de Proteção de Dados Pessoais (LGPD) – Lei nº 13.709/2018. Ao utilizar o Neocare, você, profissional da saúde, concorda com os termos e condições descritos neste documento.
+
+2. Coleta e Tratamento de Dados Pessoais
+2.1. Dados Coletados
+O Neocare coleta e processa dados pessoais dos pacientes, inseridos por médicos e profissionais de saúde, incluindo:
+
+Dados de Identificação dos Pacientes: Nome, CPF, data de nascimento, gênero, endereço, telefone e dados relacionados ao responsável pelo paciente.
+Dados de Saúde: Informações médicas relevantes, incluindo prontuários, sinais vitais, sintomas, diagnósticos, tratamentos, vacinação e acompanhamento nutricional e de desenvolvimento.
+Dados de Atendimentos: Histórico de consultas, datas e detalhes de atendimento, medicamentos prescritos e dosagens.
+Dados Profissionais: Identificação do médico ou profissional responsável pelo atendimento.
+2.2. Finalidade do Tratamento
+Os dados coletados têm como finalidades:
+
+A prestação de serviços de saúde aos pacientes neonatais, permitindo o acompanhamento adequado e eficiente de cada caso.
+A criação de prontuários e relatórios médicos para melhorar a qualidade do atendimento.
+A realização de estudos e análises estatísticas, sempre de forma anônima, para fins de pesquisa científica na área de saúde neonatal.
+3. Compartilhamento de Dados
+Os dados dos pacientes coletados no Neocare poderão ser acessados e compartilhados exclusivamente com:
+
+Profissionais de saúde responsáveis pelo acompanhamento e atendimento do paciente.
+Autoridades sanitárias e reguladoras, conforme exigido por lei ou para garantir o cumprimento de obrigações legais e regulatórias.
+Em nenhum momento os dados serão compartilhados com terceiros sem o devido consentimento do profissional de saúde responsável, exceto em situações de emergência médica ou por determinação judicial.
+
+4. Segurança dos Dados
+O Neocare implementa medidas de segurança técnicas e administrativas adequadas para proteger os dados pessoais dos pacientes contra acessos não autorizados, destruição, perda, alteração, ou qualquer outro tratamento irregular.
+
+Os dados serão mantidos por todo o período necessário para o atendimento médico dos pacientes ou conforme exigido por normas regulatórias.
+
+5. Direitos dos Pacientes
+Conforme a LGPD, os pacientes têm o direito de solicitar:
+
+Acesso aos dados coletados sobre eles.
+Correção de dados pessoais incompletos, inexatos ou desatualizados.
+Anonimização, bloqueio ou eliminação de dados desnecessários, excessivos ou tratados em desconformidade com a LGPD.
+Portabilidade dos dados a outro prestador de serviço de saúde, mediante requisição expressa e por meios seguros.
+Essas solicitações devem ser realizadas pelo médico ou profissional responsável que inseriu os dados, o qual deverá responder às solicitações dos pacientes conforme as disposições da LGPD.
+
+6. Responsabilidade dos Profissionais de Saúde
+Os profissionais de saúde que utilizam o Neocare comprometem-se a utilizar a plataforma de forma ética e responsável, respeitando o sigilo médico e a privacidade dos pacientes. É responsabilidade do profissional assegurar que os dados inseridos são precisos e atualizados.
+
+7. Alterações nos Termos de Consentimento
+O Neocare reserva-se o direito de atualizar estes Termos de Consentimento e Aceite periodicamente, conforme necessário para garantir conformidade com leis e regulamentos vigentes.
+
+8. Contato
+Para dúvidas ou solicitações sobre estes termos ou o tratamento de dados pessoais no Neocare, entre em contato com nossa equipe de suporte através do e-mail [email de contato] ou pelo telefone [número de contato].
+
+Data de última atualização: 08/01/2024.
