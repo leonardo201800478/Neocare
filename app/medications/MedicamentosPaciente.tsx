cd6b45bfc86602f0f818ff73eff173020163d806
leonardo201800478 <201800478@unifoa.edu.br>
2024-10-13 23:40:23 -0300

    Correção de dados não completos para abrir a tela de calculadora de medicamentos;

--- app/medications/MedicamentosPaciente.tsx
+++ app/medications/MedicamentosPaciente.tsx
@@ -13,31 +13,30 @@ const MedicamentosPaciente = ({ patientId }: { patientId: string }) => {
     fetchMedicationsByPatient(patientId);
   }, [patientId]);
 
-  // Função para navegar para a tela de cálculo de medicação
+  // Função para navegar para a tela de cálculo de medicação, mesmo sem dados completos
   const handleNavigateToCalculation = () => {
     if (patientId) {
       router.push(
         `/medications/CalculadoraMedicamentos/?patientId=${patientId}` as unknown as `${string}:${string}`
       );
     } else {
-      Alert.alert('Erro', 'Dados insuficientes para registrar uma vacinação.');
+      Alert.alert('Erro', 'ID do paciente não encontrado.');
     }
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.header}>Medicamentos do Paciente</Text>
-      {medications.length > 0 ? (
-        medications.map((medication) => (
-          <View key={medication.id} style={styles.medicationItem}>
-            <Text style={styles.label}>{medication.name}</Text>
-            <Text>Dosagem: {medication.dosage_info}</Text>
-            <Text>Indicações: {medication.indication}</Text>
-          </View>
-        ))
-      ) : (
-        <Text>Nenhum medicamento registrado.</Text>
-      )}
+      {medications.length > 0
+        ? medications.map((medication) => (
+            <View key={medication.id} style={styles.medicationItem}>
+              <Text style={styles.label}>{medication.name}</Text>
+              <Text>Dosagem: {medication.dosage_info}</Text>
+              <Text>Indicações: {medication.indication}</Text>
+            </View>
+          ))
+        : // Nenhum aviso para a ausência de medicamentos
+          null}
       <TouchableOpacity style={styles.button} onPress={handleNavigateToCalculation}>
         <Text style={styles.buttonText}>Calcular Medicação</Text>
       </TouchableOpacity>
